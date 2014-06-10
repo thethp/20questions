f172c526be94235abacb207dfc05b0bc74455ad4
thethp <todderick@gmail.com>
2014-06-10 10:55:36 -0400

    update logic and fix json

--- 20q.js
+++ 20q.js
@@ -1,6 +1,11 @@
 TwentyQuestions();
 
 function TwentyQuestions() {
-  this.animals = $.getJSON('animals.json');
-  console.log(this.animals);
+  this.animals;
+  $.getJSON('animals.json', function(json) {
+    this.animals = json;
+    $.each(this.animals.animals, function(i,el) {
+      console.log(el["class"]=="mammal");
+    })
+  });
 };
